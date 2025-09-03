bec6ac5f31d4a1c6ae23d24286ae1df937fded72
joshmarinacci <joshua@marinacci.org>
2025-09-02 17:30:53 -0700

    add test app

---
+++ esptest/src/main.rs
@@ -0,0 +1,120 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+
+use embedded_hal_bus::spi::ExclusiveDevice;
+use esp_hal::clock::CpuClock;
+use esp_hal::delay::Delay;
+use esp_hal::gpio::Level::{High, Low};
+use esp_hal::gpio::{Input, InputConfig, Output, OutputConfig, Pull};
+use esp_hal::main;
+use esp_hal::spi::master::{Config as SpiConfig, Spi};
+use esp_hal::time::{Duration, Instant, Rate};
+use log::info;
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::Rgb565,
+    prelude::*,
+    text::Text,
+};
+use mipidsi::interface::SpiInterface;
+use mipidsi::options::{ColorInversion, ColorOrder, Orientation, Rotation};
+use mipidsi::{models::ST7789, Builder};
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+extern crate alloc;
+
+#[main]
+fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(size: 72 * 1024);
+
+    let mut delay = Delay::new();
+
+    // have to turn on the board and wait 500ms before using the keyboard
+    let mut board_power = Output::new(peripherals.GPIO10, High, OutputConfig::default());
+    board_power.set_high();
+    delay.delay_millis(1000);
+
+    // ==== display setup ====
+    // https://github.com/Xinyuan-LilyGO/T-Deck/blob/master/examples/HelloWorld/HelloWorld.ino
+
+    let mut TFT_CS = Output::new(peripherals.GPIO12, High,
+                                 OutputConfig::default());
+    TFT_CS.set_high();
+    let tft_dc = Output::new(peripherals.GPIO11, Low,
+                             OutputConfig::default());
+    let mut tft_enable = Output::new(peripherals.GPIO42, High,
+                                     OutputConfig::default());
+    tft_enable.set_high();
+
+    let spi = Spi::new(
+        peripherals.SPI2,
+        SpiConfig::default().with_frequency(Rate::from_mhz(40)),
+    ).unwrap()
+        .with_sck(peripherals.GPIO40)
+        .with_miso(Input::new(peripherals.GPIO38,
+                              InputConfig::default().with_pull(Pull::Up)))
+        .with_mosi(peripherals.GPIO41);
+
+    let mut buffer = [0u8; 512];
+
+    info!("setting up the display");
+    let spi_delay = Delay::new();
+    let spi_device = ExclusiveDevice::new(spi, TFT_CS, spi_delay).unwrap();
+    let di = SpiInterface::new(spi_device, tft_dc, &mut buffer);
+    info!("building");
+    let mut display = Builder::new(ST7789, di)
+        .display_size(240, 320)
+        .invert_colors(ColorInversion::Inverted)
+        .color_order(ColorOrder::Rgb)
+        .orientation(Orientation::new().rotate(Rotation::Deg90))
+        .init(&mut delay)
+        .unwrap();
+
+    info!("initialized display");
+    // wait for everything to boot up
+    // delay.delay_millis(500);
+    let colors = [
+        Rgb565::BLACK,
+        Rgb565::WHITE,
+        Rgb565::RED,
+        Rgb565::GREEN,
+        Rgb565::BLUE,
+    ];
+    let style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+    for _ in 1..10 {
+        for color in colors.iter() {
+            display.clear(*color).unwrap();
+            Text::new("Hello Rust!", Point::new(20, 30), style)
+                .draw(&mut display)
+                .unwrap();
+            info!("color {:?}", *color);
+            delay.delay_millis(1000);
+        }
+    }
+
+    info!("Display initialized");
+
+    loop {
+        info!("sleeping");
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+}
