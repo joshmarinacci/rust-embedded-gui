2124699d6f744cb7c3e0b5e8018df61f4c81262b
Josh Marinacci <joshua@marinacci.org>
2025-09-11 11:51:10 -0500

    fix text input focused drawing

--- src/comps.rs
+++ src/comps.rs
@@ -90,12 +90,12 @@ pub fn make_label<C, F>(name: &str, title: &str) -> View<C, F> {
 
 fn draw_text_input<C, F>(e:&mut DrawEvent<C, F>) {
     e.ctx.fill_rect(&e.view.bounds, &e.theme.bg);
+    e.ctx.stroke_rect(&e.view.bounds, &e.theme.fg);
     if let Some(focused) = e.focused {
         if focused == &e.view.name {
+            e.ctx.stroke_rect(&e.view.bounds.contract(2), &e.theme.fg);
         }
     }
-    e.ctx.stroke_rect(&e.view.bounds, &e.theme.fg);
-    e.ctx.stroke_rect(&e.view.bounds.contract(2), &e.theme.fg);
     e.ctx.fill_text(&e.view.bounds, &e.view.title, &e.theme.fg, &HAlign::Left);
 }
 
